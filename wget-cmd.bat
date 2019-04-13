4b793a0954fb6d5c8a631f8d79e3f3a5f07873c3
Andrey Mishchenko <msav@msav.ru>
2019-04-13 12:34:03 +0300

    Added src & bin folders

--- wget-cmd.bat
+++ wget-cmd.bat
@@ -2,13 +2,18 @@
 set /p URL=Enter URL (without http://):
 set /p DNAME=Enter subdirectory:
 
-set RESDIR=%CD%\%DNAME%\
+set SRCDIR=%CD%\src
+if not exist %SRCDIR% (
+	mkdir %SRCDIR%
+)
+
+set RESDIR=%SRCDIR%\%DNAME%\
 if not exist %RESDIR% (
   mkdir %RESDIR%
 )
 
 if "%URL%"=="" goto error
-wget64.exe -r --level=5 -k -np -p -nv -nH -E -t 4 -N -c http://%URL% -P %RESDIR%
+.\bin\wget64.exe -r --level=5 -k -np -p -nv -nH -E -t 4 -N -c http://%URL% -P %RESDIR%
 goto end
 :error
 echo No site specified!
